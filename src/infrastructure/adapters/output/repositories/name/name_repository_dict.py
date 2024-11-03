e16d161125604d46cd4bfa703716a54f94899c14
Daniel Montoya <carlos.danmontoya@gmail.com>
2024-11-03 17:29:02 -0500

    Adding logs

--- src/infrastructure/adapters/output/repositories/name/name_repository_dict.py
+++ src/infrastructure/adapters/output/repositories/name/name_repository_dict.py
@@ -1,9 +1,13 @@
 import csv
+import logging
 
 from injector import singleton
 
 from src.app.ports.output.repositories.name_repository import NameRepository
 from src.domain.model.name import Name
+
+
+logger = logging.getLogger("uvicorn")
 
 
 @singleton
@@ -17,4 +21,5 @@ class NameRepositoryDict(NameRepository):
             self.__db = {row["ID"]: Name(row["ID"], row["Full Name"]) for row in data}
 
     def get_all(self) -> list[Name]:
+        logger.info("Getting all names")
         return list(self.__db.values())
